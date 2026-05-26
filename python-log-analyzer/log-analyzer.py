f886ea9874118c2f1f27f9a0051dd26d58eed9f6
Tonia <toniadane2018@gmail.com>
2026-05-26 14:01:53 +0100

    Added alert classification to log analyzer

--- python-log-analyzer/log-analyzer.py
+++ python-log-analyzer/log-analyzer.py
@@ -16,9 +16,9 @@ count = 0
 # print(f"we have {count} failed attempts")
 
 ip_list = []
+values = {}
 
 def freq_count(arr):
-    values = {}
     for i in arr:
         if values.get(i):
             value = values.get(i) + 1
@@ -27,7 +27,6 @@ def freq_count(arr):
             values.update({i:1})
     return values
 
-
 with open("sample_logs.txt", "r") as file:
     for item in file:
         splitted = item.split()
@@ -35,8 +34,15 @@ with open("sample_logs.txt", "r") as file:
         if splitted[4] == "failed":
             ip_part = splitted[6]
             ip_address = ip_part.split("=")[1]
-
-            print(ip_address)
             ip_list.append(ip_address)
 print(freq_count(ip_list))
-        
+
+# print(values)
+for key, value in values.items():
+    # print(f"{key} has made {value} attempt(s)")
+    if value >= 4:
+        print(f"[ALERT] {key} has made {value} attempt(s)")
+    elif value == 3:
+        print(f"[WARNING] {key} has made {value} attempt(s)")
+    else:
+        print(f"[INFO] {key} has made {value} attempt(s)")
